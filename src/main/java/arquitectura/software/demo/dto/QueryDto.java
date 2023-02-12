dca68887f236f939722b54493f15b35c58648884
EllieT7 <elizabeth.tacachira@gmail.com>
2023-02-12 14:16:23 -0400

    Alternativa de QueryDto funcional

--- src/main/java/arquitectura/software/demo/dto/QueryDto.java
+++ src/main/java/arquitectura/software/demo/dto/QueryDto.java
@@ -2,22 +2,25 @@ package arquitectura.software.demo.dto;
 
 import java.math.BigDecimal;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+@JsonIgnoreProperties(ignoreUnknown = true)
 public class QueryDto {
     
     private String from;
     private String to;
     private BigDecimal amount;
 
-    //Constructor QueryDto
-    public QueryDto(String from, String to, BigDecimal amount) {
-        this.from = from;
-        this.to = to;
-        this.amount = amount;
-    }
     
     //Constructor vacio
     public QueryDto() {
     }
+
+    //Constructor con parametros
+    public QueryDto(String from, String to, BigDecimal amount) {
+        this.from = from;
+        this.to = to;
+        this.amount = amount;
+    }   
 
     //toString
     @Override
--- src/main/java/arquitectura/software/demo/dto/ResponseDto.java
+++ src/main/java/arquitectura/software/demo/dto/ResponseDto.java
@@ -1,6 +1,7 @@
 package arquitectura.software.demo.dto;
 
 import java.math.BigDecimal;
+import java.util.LinkedHashMap;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
@@ -19,13 +20,13 @@ public class ResponseDto {
     //Constructor vacio
     public ResponseDto() {
     }
-    //Constructor con parametros
-    public ResponseDto(Boolean success, QueryDto query, String date, BigDecimal result) {
-        this.success = success;
-        this.query = query;
-        this.date = date;
-        this.result = result;
-    }
+    // //Constructor con parametros
+    // public ResponseDto(Boolean success, QueryDto query, String date, BigDecimal result) {
+    //     this.success = success;
+    //     this.query = query;
+    //     this.date = date;
+    //     this.result = result;
+    // }
 
     //Getters and Setters
     public String getDate() {
@@ -50,13 +51,13 @@ public class ResponseDto {
         return query;
     }
     @JsonProperty("query")
-    public void setQuery(Object query){
+    public void setQuery(LinkedHashMap query){
         //mapper
         ObjectMapper mapper = new ObjectMapper();
-        System.out.println("query: " + query);
+        System.out.println("query: " + query.toString());
         System.out.println("query.class " + query.getClass());
         try {
-            this.query = mapper.convertValue(query, QueryDto.class);
+            this.query = new QueryDto(query.get("from").toString(), query.get("to").toString(), new BigDecimal(query.get("amount").toString()));
         } catch (Exception e) {
             e.printStackTrace();
             System.out.println("Error al parsear el objeto query");
